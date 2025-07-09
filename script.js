e9b02cda1d4b4273c604c4d7b25a5038ce8a7906
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-07-09 17:25:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue(e) {
+  e.preventDefault(); // Prevent form from refreshing
+  const fname = document.getElementById("fname").value.trim();
+  const lname = document.getElementById("lname").value.trim();
+  alert(`${fname} ${lname}`);
 }
+
+const submitbtn = document.getElementById("form1");
+submitbtn.addEventListener("submit", getFormvalue);  // Use 'submit' event
